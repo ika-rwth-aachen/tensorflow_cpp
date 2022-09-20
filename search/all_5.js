10701446bb015cb36be2d6dd086a6abafe227b8c
lreiher <lreiher@users.noreply.github.com>
2022-09-20 08:51:10 +0000

    deploy: 695e59d437f01acb9d695d0ae48cd9b229a918e7

--- search/all_5.js
+++ search/all_5.js
@@ -3,7 +3,7 @@ var searchData=
   ['loadfrozengraph_0',['loadFrozenGraph',['../namespacetensorflow__cpp.html#a6afdfdf9ef167c909b705e4e39fce741',1,'tensorflow_cpp']]],
   ['loadfrozengraphintonewsession_1',['loadFrozenGraphIntoNewSession',['../namespacetensorflow__cpp.html#aa2cdf9e2b30cb9efba02d37a4dc74e7c',1,'tensorflow_cpp']]],
   ['loadgraphintosession_2',['loadGraphIntoSession',['../namespacetensorflow__cpp.html#adab6de31632607a5da1f08cc97c28aa7',1,'tensorflow_cpp']]],
-  ['loadmodel_3',['loadModel',['../classtensorflow__cpp_1_1Model.html#a7320c265ad196c71a0675fc07ec24888',1,'tensorflow_cpp::Model']]],
+  ['loadmodel_3',['loadModel',['../classtensorflow__cpp_1_1Model.html#ad0f87ba855d346a1cd0571171894d52a',1,'tensorflow_cpp::Model']]],
   ['loadsavedmodel_4',['loadSavedModel',['../namespacetensorflow__cpp.html#a610d559c69947541b90f6419992f4960',1,'tensorflow_cpp']]],
   ['loadsavedmodelintonewsession_5',['loadSavedModelIntoNewSession',['../namespacetensorflow__cpp.html#a45f336eb482f0f172b72476d8189e91d',1,'tensorflow_cpp']]]
 ];
--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['makesessionoptions_0',['makeSessionOptions',['../namespacetensorflow__cpp.html#ae9eb8935e575ad8a9b2a0cd74ed2d9a4',1,'tensorflow_cpp']]],
-  ['model_1',['Model',['../classtensorflow__cpp_1_1Model.html#aa06d09797b8c68074ad067e992359796',1,'tensorflow_cpp::Model::Model()'],['../classtensorflow__cpp_1_1Model.html#a2039875fde5fbb4e288878528db4a0a0',1,'tensorflow_cpp::Model::Model(const std::string &amp;filepath, const bool allow_growth=true, const double per_process_gpu_memory_fraction=0, const std::string &amp;visible_device_list=&quot;&quot;)'],['../classtensorflow__cpp_1_1Model.html',1,'tensorflow_cpp::Model']]],
+  ['model_1',['Model',['../classtensorflow__cpp_1_1Model.html#aa06d09797b8c68074ad067e992359796',1,'tensorflow_cpp::Model::Model()'],['../classtensorflow__cpp_1_1Model.html#aa8df0ef2fa226f3db651c24e409ff54e',1,'tensorflow_cpp::Model::Model(const std::string &amp;model_path, const bool warmup=false, const bool allow_growth=true, const double per_process_gpu_memory_fraction=0, const std::string &amp;visible_device_list=&quot;&quot;)'],['../classtensorflow__cpp_1_1Model.html',1,'tensorflow_cpp::Model']]],
   ['model_2eh_2',['model.h',['../model_8h.html',1,'']]]
 ];
